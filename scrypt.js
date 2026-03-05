040cdc38583b7cde4feddc4269a9fe66f9d56223
Yng-860 <sachamajor09@gmail.com>
2026-03-05 13:38:05 +0100

    Add files via upload

---
+++ scrypt.js
@@ -0,0 +1,10 @@
+function colorchange(){
+    document.getElementById("par").style.color = "red";
+
+}
+function fontchange(){
+    document.getElementById("par").style.fontFamily = "Italic";
+}
+function Sizechange(){
+    document.getElementById("par").style.fontSize = "40px";
+}
